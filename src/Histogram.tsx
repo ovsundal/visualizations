6adbf7acbf5a830200909e2e333aefdd2ffbf3d4
Ove <osundal83@gmail.com>
2019-12-31 23:51:19 +0100

    add histogram test data

--- src/Histogram.tsx
+++ src/Histogram.tsx
@@ -1,19 +1,78 @@
-import * as React from 'react'
+import * as React from "react";
+import styled from "styled-components";
+import { useMemo } from "react";
+import * as d3 from "d3";
 
-interface IHistogramProps {
+interface IHistogramProps {}
 
-}
+const testData = [
+  {
+    name: "A",
+    value: 1
+  },
+  {
+    name: "B",
+    value: 5
+  },
+  {
+    name: "C",
+    value: 2
+  },
+  {
+    name: "D",
+    value: 4
+  },
+  {
+    name: "E",
+    value: 7
+  }
+];
 
 export const Histogram: React.FC = (props: IHistogramProps) => {
+  const [xScale, yScale] = useSetScales();
 
+  console.log(xScale);
+  console.log(yScale);
 
+  return (
+    <g>
+      {testData.map(bar => (
+        <HistogramBar width={200} height={100} />
+      ))}
+      <rect width={200} height={100} />
+    </g>
+  );
+};
 
-    return(
-        <g>
-          <rect
-            width={200}
-            height={100}
-          />
-        </g>
-    )
+function useSetScales() {
+  const xScale = useMemo(
+    () =>
+      d3
+        .scaleLinear()
+        .domain([0, 1000])
+        .range([0, 500]),
+    []
+  );
+
+  const yScale = useMemo(
+    () =>
+      d3
+        .scaleLinear()
+        .domain([0, 700])
+        .range([600]),
+    []
+  );
+
+  return [xScale, yScale];
 }
+
+function makeBar({ xScale, yScale }: any) {
+  const props = {
+    width: xScale,
+    height: yScale
+  };
+
+  return <HistogramBar {...props} />;
+}
+
+const HistogramBar = styled.rect``;
